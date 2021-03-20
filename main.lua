d8606babf144896119776131e7e7ddf9f05e9725
Thomas <contact@thomas-george.com>
2021-03-20 15:37:25 +0100

    move pad with mouse

--- main.lua
+++ main.lua
@@ -1,10 +1,24 @@
+require 'pad'
+
 function love.load()
+  
+  -- Window size
+  windowWidth = love.graphics.getWidth()
+  windowHeight = love.graphics.getHeight()
+  
+  -- place pad to bottom
+  pad.positionY = windowHeight - pad.height
+  
 end
 
 
 function love.update(dt)
+  
+  pad.positionX = love.mouse.getX() - (pad.width / 2)
+  
 end
 
 
 function love.draw()
+  love.graphics.rectangle('fill', pad.positionX, pad.positionY, pad.width, pad.height) 
 end
---
+++ pad.lua
@@ -0,0 +1,5 @@
+pad = {}
+pad.width = 80
+pad.height = 20
+pad.positionX = 0
+pad.postionY = 0
