431546c6be63329e825ba2f58820aff99dff2d94
Thomas <contact@thomas-george.com>
2021-03-20 16:59:12 +0100

    ball rebound

--- ball.lua
+++ ball.lua
@@ -3,3 +3,5 @@ ball.positionX = 0
 ball.positionY = 0
 ball.radius = 10
 ball.sticky = false
+ball.speedX = 0
+ball.speedY = 0
--- main.lua
+++ main.lua
@@ -3,7 +3,6 @@ require 'ball'
 require 'startGame'
 
 function love.load()
-  
   -- Window size
   windowWidth = love.graphics.getWidth()
   windowHeight = love.graphics.getHeight()
@@ -17,15 +16,43 @@ end
 
 
 function love.update(dt)
-  
   pad.positionX = love.mouse.getX()
   
-  -- stick ball to the pad
+  -- ball state
   if ball.sticky == true then
     ball.positionX = pad.positionX
     ball.positionY = pad.positionY - (ball.radius + (pad.height / 2))
+  else
+    ball.positionX = ball.positionX + (ball.speedX * dt)
+    ball.positionY = ball.positionY - (ball.speedY * dt)
   end
-
+  
+  -- ball rebound
+  if (ball.positionX + ball.radius) >= windowWidth then
+    ball.speedX = -ball.speedX
+    ball.positionX = (windowWidth - ball.radius)
+  end
+  
+  if (ball.positionX - ball.radius) <= 0 then
+    ball.speedX = -ball.speedX
+    ball.positionX = (0 + ball.radius)
+  end
+  
+  if (ball.positionY - ball.radius) <= 0 then
+    ball.speedY = -ball.speedY
+    ball.positionY = (0 + ball.radius)
+  end
+  
+  if (ball.positionY + ball.radius) >= windowHeight then
+    start()
+  end
+  
+  if (ball.positionY + ball.radius) >= (pad.positionY - (pad.height / 2)) 
+  and ball.positionX >= (pad.positionX - (pad.width / 2)) 
+  and ball.positionX <= (pad.positionX + (pad.width / 2)) then
+    ball.speedY = -ball.speedY
+    ball.positionY = (pad.positionY - (pad.height / 2)) - ball.radius
+  end
 end
 
 
@@ -35,4 +62,14 @@ function love.draw()
   
   -- ball
   love.graphics.circle('fill', ball.positionX, ball.positionY, ball.radius)
+end
+
+
+function love.mousepressed(x, y, n)
+  if ball.sticky == true then
+    ball.sticky = false
+    
+    ball.speedX = 200
+    ball.speedY = 200
+  end
 end
